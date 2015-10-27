52f78a0c0c4464f12a4f0484a5663310df0ffa6c
Adam Novak <anovak@soe.ucsc.edu>
2015-10-27 12:25:10 -0700

    Add a main file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp: Main file for core graph merger
+
+#include <iostream>
+
+#include "ekg/xg/xg.hpp"
+#include "benedictpaten/pinchesAndCacti/inc/stPinchGraphs.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
